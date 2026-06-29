9564219bdc43f294d4ad2f8518b4f29594cb0e9d
prodigy3211 <37477839+Prodigy3211@users.noreply.github.com>
2026-06-29 18:46:54 -0400

    Add ability to edit member profile
    fixed some issues with data rendering and database queries.
    They all should be under the lib folder now.

--- summaview/lib/members.ts
+++ summaview/lib/members.ts
@@ -1,15 +1,44 @@
 import {pool} from "@/lib/db";
-import { Nerko_One } from "next/font/google";
-import { Result } from "pg";
 
 
+// Get all the members
 
-export async function getMembers() {
-    const result = await pool.query(`
+export async function getMembers(query: string ="") {
+    const search = `%${query}`;
+    const result = query ? await pool.query(`
         SELECT id, first_name, last_name, email, phone, status
         FROM members
-        ORDER BY id DESC`);
+        WHERE first_name ILIKE $1
+            OR last_name ILIKE $1
+            OR email ILIKE $1
+            OR phone ILIKE $1
+            OR status ILIKE $1
+        ORDER BY last_name ASC`,
+        [search]
+    )
+
+: await pool.query(
+    `
+    SELECT id, first_name, last_name, phone, email, status
+    FROM members
+    ORDER BY last_name ASC
+    `
+);
     return result.rows;
+}
+
+//Get specific member
+
+export async function getMembersById(id: string) {
+    const result = await pool.query(
+        `
+        SELECT id, first_name, last_name, email, phone, status
+        FROM members
+        WHERE id = $1
+        `,
+        [id]
+    );
+    return result.rows[0];
 }
 
 //Inserts a new member into the Database, connects with the new folder under membersearch
@@ -32,13 +61,7 @@ export async function createMember(member: CreateMember){
         phone,
         status
         )
-        VALUES (
-        $1,
-        $2,
-        $3,
-        $4,
-        $5
-        )
+        VALUES ($1, $2, $3, $4, $5)
         RETURNING id 
         `,
         [
@@ -47,6 +70,41 @@ export async function createMember(member: CreateMember){
         member.email,
         member.phone,
         member.status,
+        ]
+    );
+    return result.rows[0];
+}
+
+//Update Member
+
+export type UpdateMember = {
+    firstName: string;
+    lastName: string;
+    email: string;
+    phone: string;
+    status: string;
+}
+
+export async function updateMember(id: string, member: UpdateMember) {
+    const result = await pool.query (
+        `
+        UPDATE members
+        SET
+            first_name =$1,
+            last_name = $2,
+            email = $3,
+            phone = $4,
+            status = $5
+        WHERE id = $6
+        RETURNING id, first_name, last_name, email, phone, status
+        `,
+        [
+            member.firstName,
+            member.lastName,
+            member.email,
+            member.phone,
+            member.status,
+            id,
         ]
     );
     return result.rows[0];
